b65ec3b3a0e5cc72d1c91bdb02f0a1fc21e785ba
Szymon Słomiński <simon.slominski@gmail.com>
2019-02-17 19:42:13 +0100

    Add endpoint to Flask

--- Solution_with_pandas_library/main.py
+++ Solution_with_pandas_library/main.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from flask import jsonify
 
+from search import find_pets
 
 app = Flask(__name__)
 
 
-@app.route('/, methods=['GET'])
-def pets_in_city():
-    return 'test Flask server'
+@app.route('/api/v1/<city>', methods=['GET'])
+def pets_in_city(city):
+    return jsonify(find_pets(city))
 
 
 if __name__ == '__main__':
